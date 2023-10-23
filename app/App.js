d68f649fb1bfd86d0395cb99be00ac448049d8c3
Joe Johnston <2680218j@student.gla.ac.uk>
2023-10-23 17:22:43 +0100

    Resolving conflicts for merge to main

--- app/App.js
+++ app/App.js
@@ -1,9 +1,121 @@
 import React, { Component, useState, useEffect } from 'react';
 import { Alert, StatusBar, StyleSheet, View, Text, Pressable, Image } from 'react-native';
+import { NavigationContainer, useNavigation, Navigate } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 StatusBar.setBarStyle('light-content');
 
-export default class QuizScreen extends Component {
+function correctAnswer() {
+    return(
+        Alert.alert("Correct")
+    )
+}
+
+function incorrectAnswer() {
+    return(
+        Alert.alert("Incorrect")
+    )
+}
+
+function HomeScreen() {
+    const navigation = useNavigation();
+    return(
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "space-evenly", backgroundColor: '#272522' }}>
+            <Image source={require('./assets/msy-logo.png')} style={{width: 200, height:400, objectFit: 'contain'}}/>
+            <Text style={styles.answerText}>Welcome to My Student Years</Text>
+            <Pressable onPress={() => navigation.navigate("Categories")}>
+            <Text style={styles.selectButton}>
+            Go to Categories
+            </Text>
+            </Pressable>
+        </View>
+    )
+}
+
+function Categories() {
+    const navigation = useNavigation();
+    return (
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "space-evenly", backgroundColor: '#272522' }}>
+      <Text style={styles.answerText}>Welcome to the Categories page</Text>
+    
+      <Pressable onPress={() => navigation.navigate("Finance")}>
+        <Text style={styles.selectButton}>Finance</Text>
+      </Pressable>
+
+      <Pressable onPress={() => navigation.navigate("Categories")}>
+        <Text style={styles.selectButton}>
+            Wellbeing
+        </Text>
+      </Pressable>
+
+      <Pressable onPress={() => navigation.navigate("Categories")}>
+        <Text style={styles.selectButton}>
+            Academic Support
+        </Text>
+      </Pressable>
+
+      <Pressable onPress={() => navigation.navigate("Categories")}>
+        <Text style={styles.selectButton}>
+            4th category(?)
+        </Text>
+      </Pressable>
+    </View>
+    );
+}
+
+function Finance() {
+    const navigation = useNavigation();
+    return(
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "space-evenly", backgroundColor: '#272522' }}>
+            <Text style={styles.answerText}>This is the finance section</Text>
+            <Text style={styles.questionText}>
+            There will be a bunch of resources here.
+            </Text>
+            <Pressable onPress={() => navigation.navigate("Finance Quiz")}>
+            <Text style={styles.selectButton}>
+            Go to the quiz for this section
+            </Text>
+            </Pressable>
+        </View>
+    )
+}
+
+function FinanceQuiz()  {
+    return(
+            <View style={styles.container}>
+                <View style={styles.questionContainer}>
+                    <Image source={require('./assets/mirror.png')} style={{width: 100, height:200, objectFit: 'contain'}}/>
+                    <Text style={styles.questionText}>Should you spend your entire student loan payment on alcohol?</Text>
+                </View>
+                <View style={styles.buttonRow}>
+                    <Pressable onPress={correctAnswer} style={[styles.answerButton, {backgroundColor: '#dd8844'}]}>
+                        <Text style={styles.answerText}>
+                        Definitely.
+                        </Text>
+                    </Pressable>
+                    <Pressable onPress={incorrectAnswer} style={[styles.answerButton, {backgroundColor: '#44dd88'}]}>
+                        <Text style={styles.answerText}>
+                        Probably?
+                        </Text>
+                    </Pressable>
+                </View>
+                <View style={styles.buttonRow}>
+                    <Pressable onPress={incorrectAnswer} style={[styles.answerButton, {backgroundColor: '#dd4488'}]}>
+                        <Text style={styles.answerText}>
+                        Maybe?
+                        </Text>
+                    </Pressable>
+                    <Pressable onPress={incorrectAnswer} style={[styles.answerButton, {backgroundColor: '#8844dd'}]}>
+                        <Text style={styles.answerText}>
+                        No?
+                        </Text>
+                    </Pressable>
+                </View>
+            </View>
+        );
+}
+
+/*export default class QuizScreen extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -67,7 +179,7 @@ export default class QuizScreen extends Component {
                 </View>
             </View>
         );
-}
+}*/
 
 const Stack = createNativeStackNavigator();
 
