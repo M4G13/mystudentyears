dcdeeeb97de3d930ed6905c0da7d02a6ecfae829
Kevin Cahill <2685130C@student.gla.ac.uk>
2023-11-08 13:51:16 +0000

    Started work on sequential pages

--- app/App.js
+++ app/App.js
@@ -15,7 +15,7 @@ export default function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const url = "http://192.168.0.23:1337/api/students?populate=deep";
+  const url = "http://172.20.10.3:1337/api/students?populate=deep";
 
   const fetchData = () => {
     setIsLoading(true);
--- app/screens/Info.js
+++ app/screens/Info.js
@@ -1,36 +1,128 @@
-import { Component, useState, useEffect } from "react";
-import { Alert, StyleSheet, View, Text, Pressable, Image } from "react-native";
-
+import React, { useState } from "react";
+import { View, Text, ScrollView, Pressable } from "react-native";
 import baseStyle from "../styles/base.js";
 
-export default function Info({ route, navigation }) {
-  const { id, category_id, student_id } = route.params;
-  const info = global.data.data
-    .find((s) => s.id === student_id)
-    .attributes.category.find((c) => c.id === category_id)
-    .information.data.find((i) => i.id === id).attributes;
+export default function Info() {
+  const students = global.data.data;
+  const [currentPage, setCurrentPage] = useState({
+    studentIndex: 0,
+    categoryIndex: 0,
+  });
+
+  const navigateToNextPage = () => {
+    if (currentPage.categoryIndex < students[currentPage.studentIndex].attributes.category.length - 1) {
+      setCurrentPage({
+        studentIndex: currentPage.studentIndex,
+        categoryIndex: currentPage.categoryIndex + 1,
+      });
+    } else if (currentPage.studentIndex < students.length - 1) {
+      setCurrentPage({
+        studentIndex: currentPage.studentIndex + 1,
+        categoryIndex: 0,
+      });
+    }
+  };
+
+  const navigateToPreviousPage = () => {
+    if (currentPage.categoryIndex > 0) {
+      setCurrentPage({
+        studentIndex: currentPage.studentIndex,
+        categoryIndex: currentPage.categoryIndex - 1,
+      });
+    } else if (currentPage.studentIndex > 0) {
+      const prevStudentIndex = currentPage.studentIndex - 1;
+      setCurrentPage({
+        studentIndex: prevStudentIndex,
+        categoryIndex: students[prevStudentIndex].attributes.category.length - 1,
+      });
+    }
+  };
+
+  const student = students[currentPage.studentIndex];
+  const category = student.attributes.category[currentPage.categoryIndex];
+  const information = category.information.data;
 
   return (
-    <View style={baseStyle.view}>
-      <Text style={baseStyle.bigText}>{info.Title}</Text>
-      <Text style={baseStyle.bigText}>{info.Text}</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <Pressable onPress={navigateToPreviousPage}>
+        <Text style={styles.button}>Previous</Text>
+      </Pressable>
+      
+      <View key={student.id} style={styles.studentContainer}>
+        <Text style={styles.studentName}>Student: {student.attributes.Name}</Text>
+        <View key={category.id} style={styles.categoryContainer}>
+          <Text style={styles.categoryName}>Category: {category.Category}</Text>
+          <View style={styles.infoContainer}>
+            {information.map((info) => (
+              <View key={info.id} style={styles.infoCard}>
+                <Text style={styles.infoTitle}>{info.attributes.Title}</Text>
+                <Text style={styles.infoText}>{info.attributes.Text}</Text>
+              </View>
+            ))}
+          </View>
+        </View>
+      </View>
+      
+      <Pressable onPress={navigateToNextPage}>
+        <Text style={styles.button}>Next</Text>
+      </Pressable>
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  mainImage: {
-    width: 200,
-    height: 400,
-    objectFit: "contain",
+const styles = {
+  container: {
+    padding: 16,
+    backgroundColor: "#f5f5f5", 
   },
-
-  selectButton: {
-    backgroundColor: "#05b4ff",
-    padding: 15,
-    paddingLeft: 25,
-    paddingRight: 25,
-    borderRadius: 5,
-    width: "45%",
+  button: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#007AFF", 
+    margin: 10,
   },
-});
+  studentContainer: {
+    marginBottom: 16,
+    backgroundColor: "#fff", 
+    padding: 16,
+    borderRadius: 8,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+    elevation: 2,
+  },
+  studentName: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 8,
+  },
+  categoryContainer: {
+    marginTop: 8,
+  },
+  categoryName: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  infoContainer: {
+    marginTop: 8,
+  },
+  infoCard: {
+    backgroundColor: "#fff", 
+    padding: 16,
+    borderRadius: 8,
+    marginBottom: 16,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+    elevation: 2,
+  },
+  infoTitle: {
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+  infoText: {
+    fontSize: 14,
+  },
+};
