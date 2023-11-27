1a76fc00f54a44e4f97b02f475bd0f92a75396ee
Kevin Cahill <2685130C@student.gla.ac.uk>
2023-11-27 03:24:31 +0000

    Added some more functionality to QuizEndScreen

--- app/screens/QuizEndScreen.js
+++ app/screens/QuizEndScreen.js
@@ -23,17 +23,29 @@ export default function QuizEndScreen({ route, navigation }) {
     navigation.navigate("Categories", { student_id });
   };
 
+  let message = "";
+
+  if (score < 50) {
+    message = "You can do better. Keep practicing!";
+  } else if (score >= 75) {
+    message = "Congratulations! You did amazing!";
+  } else {
+    message = "Good effort! You're on the right track.";
+  }
+
   return (
     <View style={baseStyle.view}>
       <Text style={baseStyle.bigText}>Quiz Completed!</Text>
-      <Text style={baseStyle.bigText}>Your Score: {score.toFixed(2)}%</Text>
-      <Text style={baseStyle.bigText}>You got {correctAmount} out of {QuestionAmount} </Text>
-      <Pressable onPress={retakeQuiz}>
-        <Text style={baseStyle.button}>Retake Quiz</Text>
-      </Pressable>
-      <Pressable onPress={goToMap}>
-        <Text style={baseStyle.button}>Go back to campus</Text>
-      </Pressable>
+      <Text style={baseStyle.smallText}>You got {score.toFixed(2)}%, which is {correctAmount} out of {QuestionAmount}</Text>
+      <Text style={baseStyle.smallText}>{message}</Text>
+      <View style={[baseStyle.buttonContainer, { flexDirection: 'row' }]}>
+        <Pressable onPress={retakeQuiz} style={[baseStyle.button, {marginRight: 5}]}>
+          <Text style={baseStyle.buttonText}>Retake Quiz</Text>
+        </Pressable>
+        <Pressable onPress={goToMap} style={baseStyle.button}>
+          <Text style={baseStyle.buttonText}>Go back to campus</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
