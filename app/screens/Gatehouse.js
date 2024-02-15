ee87a2d039e25fff2276f4c8dba7727a690dbd6f
Euan Douglas <2672049d@student.gla.ac.uk>
2024-02-15 15:54:28 +0000

    remove unused variable

--- app/screens/Gatehouse.js
+++ app/screens/Gatehouse.js
@@ -29,8 +29,6 @@ export default function Gatehouse({ navigation }) {
     left: require("../assets/left.png"),
   };
 
-  let alertString = "";
-
   const [openStories, setOpenStories] = useState({});
   useFocusEffect(
     useCallback(() => {
