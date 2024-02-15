092a64af7eefe6b40742322cedcd3473a3db1f3a
Magnus Reid <2668261r@student.gla.ac.uk>
2024-02-15 14:17:01 +0000

    make map domain automatic

--- strapi/src/admin/extensions/AnalyticsPage/SchoolMap.jsx
+++ strapi/src/admin/extensions/AnalyticsPage/SchoolMap.jsx
@@ -6,35 +6,33 @@ import * as topojson from "topojson-client";
 const url = "http://localhost:1337/api";
 
 export default function SchoolMap() {
-  const [schools, setSchools] = useState([]);
-  const [users, setUsers] = useState([]);
   const [regions, setRegions] = useState([]);
+  const [domain, setDomain] = useState([0, 0]);
 
   useEffect(() => {
     fetch(url + '/stats/laFreq')
       .then(response => response.json())
-      .then(data => setRegions(data))
-      .catch(error => console.log(error));
-    fetch(url + '/app-users?pagination[pageSize]=500&populate=*')
-      .then(response => response.json())
-      .then(data => setUsers(data.data))
+      .then(data => {
+        setRegions(data);
+        const vals = data.map(r => r.value);
+        setDomain([Math.min(...vals), Math.max(...vals)])
+      })
       .catch(error => console.log(error));
   }, []);
 
   const geojson = topojson.feature(scotland, scotland.objects.lad);
-  console.log(regions);
 
   return (
     <div style={{height: 1000}}>
       <ResponsiveChoropleth
           data={regions}
           features={geojson.features}
-          domain={[5,31]}
+          domain={domain}
           colors="RdPu"
           label='properties.LAD13NM'
           projectionScale={4000}
           projectionTranslation={[0.5, 5.4]}
-          onClick={node => console.log(node)}
+          onClick={node => console.log(node, domain, regions)}
         />
     </div>
   );
