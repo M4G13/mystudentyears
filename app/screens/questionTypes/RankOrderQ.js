9b531065b3fd184610556b98baddfba184853915
Magnus Reid <2668261r@student.gla.ac.uk>
2024-03-01 16:02:02 +0000

    make app work with eas

--- app/screens/questionTypes/RankOrderQ.js
+++ app/screens/questionTypes/RankOrderQ.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { View, Text, Pressable, TouchableOpacity } from "react-native";
-import DraggableFlatList, {
-  ScaleDecorator,
-} from "react-native-draggable-flatlist";
+// import DraggableFlatList, {
+//   ScaleDecorator,
+// } from "react-native-draggable-flatlist";
 
 import style from "../../styles/rankorderq.js";
 
@@ -11,32 +11,32 @@ const _ = require("lodash");
 export default function RankOrderQ({ question, handleAnswer }) {
   const [data, setData] = useState(_.shuffle(question.answers));
 
-  const renderItem = ({ item, drag, isActive }) => {
-    return (
-      <ScaleDecorator>
-        <TouchableOpacity
-          onLongPress={drag}
-          delayLongPress={0}
-          disabled={isActive}
-          style={style.listItem}
-        >
-          <Text style={style.bigText}>{item.answer}</Text>
-        </TouchableOpacity>
-      </ScaleDecorator>
-    );
-  };
+  // const renderItem = ({ item, drag, isActive }) => {
+  //   return (
+  //     <ScaleDecorator>
+  //       <TouchableOpacity
+  //         onLongPress={drag}
+  //         delayLongPress={0}
+  //         disabled={isActive}
+  //         style={style.listItem}
+  //       >
+  //         <Text style={style.bigText}>{item.answer}</Text>
+  //       </TouchableOpacity>
+  //     </ScaleDecorator>
+  //   );
+  // };
 
   return (
     <View style={style.questionWrapper}>
       <View style={style.questionContainer}>
         <Text style={style.bigText}>{question.question}</Text>
       </View>
-      <DraggableFlatList
+      {/*<DraggableFlatList
         data={data}
         onDragEnd={({ data }) => setData(data)}
         keyExtractor={(item) => item.id}
         renderItem={renderItem}
-      />
+      />*/}
       <View style={style.submitButtonContainer}>
         <Pressable
           style={style.submitButton}
