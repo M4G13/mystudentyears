464aadaa711f601831b296806ee7421131eeb2bb
Joe Johnston <2680218j@student.gla.ac.uk>
2024-03-11 01:14:51 +0000

    Working to solve auth issues

--- strapi/src/admin/extensions/AnalyticsPage/index.jsx
+++ strapi/src/admin/extensions/AnalyticsPage/index.jsx
@@ -2,14 +2,25 @@ import BarChart from './BarChart.jsx';
 import SchoolMap from './SchoolMap.jsx';
 import React, {useEffect} from 'react';
 import { json2csv } from 'json-2-csv';
+import axios from 'axios';
 
+import {getFetchClient} from '@strapi/helper-plugin';
 export default function Analytics() {
+  const fetchData = async () => {
+    const { get } = getFetchClient();
+    const response = await get('http://localhost:1337/api/stats/laFreq');
+    console.log(response.data);
+  }
+
+  useEffect(()=>{
+    fetchData();
+  }, []);
   return (
     <div>
-      <BarChart type="InitialSurvey" />
+      {/*<BarChart type="InitialSurvey" />
       <BarChart type="FinalSurvey" />
       <SchoolMap />
-      <a style={{backgroundColor: '#ffffff', padding: 5, borderRadius: 10, margin: 30}} href="../../api/stats/userDetails" download="UserData.csv">Download Data</a>
+      <a style={{backgroundColor: '#ffffff', padding: 5, borderRadius: 10, margin: 30}} href="../../api/stats/userDetails" download="UserData.csv">Download Data</a>*/}
     </div>
   );
 }
