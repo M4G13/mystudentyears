0f62a599430278af2d062cbc816eb68643f1e6c5
Taylor Scott <2670341s@student.gla.ac.uk>
2024-03-12 13:36:52 +0000

    Fixed components/Grade.js

--- app/components/Grade.js
+++ app/components/Grade.js
@@ -35,7 +35,7 @@ export function GradeIcon({ style, score }) {
         source={require("../assets/gradeCircle.png")}
         style={styleSheet.gradeImage}
       />
-       style={styleSheet.gradeText} adjustsFontSizeToFit>
+      <Text style={styleSheet.gradeText} adjustsFontSizeToFit>
         {calculateGrade(score)}
       </Text>
     </View>
